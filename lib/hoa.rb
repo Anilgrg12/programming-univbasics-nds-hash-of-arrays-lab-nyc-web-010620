e5a3b29ff75ef843801c86254a22c28807b41f36
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 01:20:38 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,11 +6,6 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  BASE_HOA = {
-  :chipmunks => ["Alvin", "Simon", "Theodore"],
-  :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
-  :jetsons => ["George", "Jane", "Judy", "Elroy"]
-}
   BASE_HOA[:show] << "name"
   BASE_HOA
   # Write your implementation here
